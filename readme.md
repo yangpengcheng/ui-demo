44a0c7916a0000d1c7ce230bdbff38e15b58cf07
yangpengcheng <550017988@qq.com>
2021-05-06 22:21:49 +0800

    0.1

--- readme.md
+++
@@ -1,5 +0,0 @@
-### ypc-ui demo
-#### npm package
-```
-https://www.npmjs.com/package/ypc-ui
-```
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import ypcui from 'ypc-ui'
-import 'ypc-ui/dist/ypc-ui.css'
-
-
-createApp(App).use(router).use(ypcui).mount('#app')
